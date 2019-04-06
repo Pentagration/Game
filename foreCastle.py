6d7c98007035ee8f67fdd6c080b62fef098a1607
jasonpettit <46112312+jasonpettit@users.noreply.github.com>
2019-04-06 11:21:02 -0700

    Update foreCastle.py

--- foreCastle.py
+++ foreCastle.py
@@ -1,14 +1,14 @@
-def foreCastle():
+def foreCastle(turtle):
   #remove the options that are NOT valid for your room
   valid = ['r','l','b']
   
   #Add the story about your room
   print "You are in the Forecastle where the Baker, Tanner,"
-  print "and Sail Maker work."
+  print "and Sail Maker work.\n"
   
   print "(R)IGHT: The Baker"
   print "(L)EFT: The Tanner "
-  print "(B)ACK: The Deck"
+  print "(B)ACK: The Deck\n"
   
   #see what they decided to do
   result = choice(valid)
@@ -16,13 +16,19 @@ def foreCastle():
   #based on the choice return where does the player move to?
   #CALL LOCATION FROM SETUP
   if result == 'r':
-    print "This is our area. Go back to the deck."
+    print "BAKER SAYS: This is our area. Go back to the deck."
+    print "He pushes you back out the door. \n"
+    name = "deck"
+    location(turtle,name)
     return name
   elif result == 'l':
-    print "This is our area. Go back to the deck."
+    print "TANNER SAYS: This is our area. Go back to the deck."
+    print "He pushes you back out the door. \n"
+    name = "deck"
+    location(turtle,name)
     return name
   elif result == 'b':
     name = "deck"
     location(turtle,name)
     return name
-#end room                                                                     
+#end foreCastle   
